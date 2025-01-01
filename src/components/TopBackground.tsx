aa4a1c0c4943c164366b763d5ef5981ce363a366
changuii <rhljh7410@gmail.com>
2025-01-01 17:14:22 +0900

    feat : Introduce page 완료

---
+++ src/components/TopBackground.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export interface TopBackgroundProps {
+    url : string;
+}
+
+const TopBackground: React.FC<TopBackgroundProps> = ({url}) => {
+
+    return (
+        <div className="absolute top-0 left-0 w-full h-4/5 z-[-1]">
+            <img src={`/${url}`} alt="" className="w-full h-full object-cover"/>
+        </div>
+    );
+}
+
+
+export default TopBackground;
--- src/pages/Introduce.tsx
+++ src/pages/Introduce.tsx
@@ -1,9 +1,45 @@
 import React from "react";
+import TopBackground from "../components/TopBackground.tsx";
 
 
 const Introduce: React.FC = () => {
+    const url: string = 'introduce-background.jpg';
+
     return (
-        <></>
+        <>
+            <TopBackground url={url}/>
+            <div className='w-full h-4/5 flex flex-col items-center justify-center'>
+                <div className='text-white m-10 text-6xl font-bold'>Introduction Lab</div>
+            </div>
+            <div className='w-full h-1/5 flex items-center justify-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                     stroke="currentColor" className="size-8">
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                          d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"/>
+                </svg>
+            </div>
+            <div className='w-full flex flex-col justify-center items-center'>
+                <div className='w-2/3  m-20'>
+                    <div className='text-xl font-bold text-gray-900'>
+                        In the 21st century with information and telecommunication more important than ever before, the
+                        mobile network field is rapidly developing as the core of the information and telecommunication
+                        field.
+                    </div>
+                    <div className='mt-10 text-lg font-bold text-gray-500'>
+                        The importance of information security is being more emphasized, as we live in the information
+                        society with system development. Information security is a social and national issue rather than
+                        personal. Under these circumstances, we have established Next-Generation Information Security
+                        Laboratory(NISL). "Next-Generation Information Security Technology" includes internet of
+                        things(IoT) security, vehicle ad hoc network(VANET) security, FinTech security, information
+                        security service technology for the mobile Internet, and authentication technology for the
+                        various network security system. These are the core of the next-generation network security
+                        technology. NISL at Keimyung University will do its best to improve the information security
+                        field in Korea, meet international standards, and enhance its competitive edge in the rapidly
+                        changing information and telecommunication society.
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
