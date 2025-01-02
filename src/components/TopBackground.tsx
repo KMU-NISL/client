58b2fa72203be7534407baff5a7a1746d36528f3
changuii <rhljh7410@gmail.com>
2025-01-02 22:18:19 +0900

    feat : 각 페이지별 배경 사진 위에 검은색을 씌워 글씨를 더 잘보이도록 하라

--- src/components/TopBackground.tsx
+++ src/components/TopBackground.tsx
@@ -7,9 +7,12 @@ export interface TopBackgroundProps {
 const TopBackground: React.FC<TopBackgroundProps> = ({url}) => {
 
     return (
-        <div className={`absolute top-0 left-0 w-full h-4/5 z-[-1]`}>
-            <img src={`/${url}`} alt="" className="w-full h-full object-cover"/>
-        </div>
+        <>
+            <div className={`absolute top-0 left-0 w-full h-4/5 z-[-1]`}>
+                <img src={`/${url}`} alt="" className="w-full h-full object-cover"/>
+            </div>
+            <div className='absolute top-0 left-0 w-full h-4/5 z-[-1] bg-black opacity-40'></div>
+        </>
     );
 }
 
